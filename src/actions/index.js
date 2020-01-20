394c3ad0a280d3ea50b70e1ef295bfe8c2e54cfe
Rafael Barbosa de Sousa <rafael.barbosa@aluno.unb.br>
2020-01-20 18:16:12 -0300

    Projeto de blogs - REACT

---
+++ src/actions/index.js
@@ -0,0 +1,44 @@
+import _ from 'lodash';
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPostsAndUsers = () => async (dispatch,getState) => {
+    await dispatch(fetchPosts());
+    
+    // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    // userIds.forEach(id => dispatch(fetchUser(id)));
+
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value();
+
+};
+
+export const fetchPosts = () => async dispatch => {
+    const response = await jsonPlaceholder.get('/posts');
+
+    dispatch( {
+        type: 'FETCH_POSTS',
+        payload: response.data
+    });
+}
+
+export const fetchUser = userId => async dispatch => {
+    const response = await jsonPlaceholder.get(`/users/${userId}`);
+
+    dispatch({
+        type: 'FETCH_USER',
+        payload: response.data
+    });
+}
+
+// export const fetchUser = userId => dispatch => _fetchUser(userId,dispatch);
+// const _fetchUser = _.memoize( async (userId,dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${userId}`);
+
+//     dispatch({
+//         type: 'FETCH_USER',
+//         payload: response.data
+//     });
+// });
